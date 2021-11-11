43110c32c1eb0657f7e28c177da15bd934dcb487
azaz ahmed <ahsanlisan2@.com>
2021-11-11 19:47:34 +0600

    server push

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const ObjectId = require("mongodb").ObjectId;
+
 const { MongoClient } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.llkrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -18,7 +23,53 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("database connected successfully");
+
+    const database = client.db("NicheStore");
+    const productsCollection = database.collection("products");
+    // order seperate collection
+    const ordersCollection = database.collection("orders");
+
+    //POst APi
+    app.post("/addProduct", async (req, res) => {
+      const result = await productsCollection.insertOne(req.body);
+      console.log(result);
+      // Send Api
+      res.send(result);
+    });
+    // Get Product Api
+
+    app.get("/allProducts", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // Get Single Product
+    app.get("/SingleProduct/:id", async (req, res) => {
+      const result = await productsCollection
+        .find({ _id: ObjectId(req.params.id) })
+        .toArray();
+      res.send(result[0]);
+      console.log(result[0]);
+    });
+    // Confirm Order
+    app.post("/ConfirmOrder", async (req, res) => {
+      const result = await ordersCollection.insertOne(req.body);
+      res.send(result);
+    });
+    //My Orders
+    app.get("/myOrders/:email", async (req, res) => {
+      const result = await ordersCollection
+        .find({ email: req.params.email })
+        .toArray();
+      res.send(result);
+    });
+    //Delete Order
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const result = await ordersCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
